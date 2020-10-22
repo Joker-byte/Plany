76f2d4d117d5c002fff11bd8086a5cee3c00d828
Joker-byte <gianlucadubioso@yahoo.com>
2020-10-22 12:46:31 +0200

    impostazione di base

--- Plany/ViewController.swift
+++ Plany/ViewController.swift
@@ -8,7 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var HelloLabel: UILabel!
+    
+    @IBOutlet weak var NameLabel: UILabel!
+    
+    
+    @IBOutlet weak var ProfileImage: UIImageView!
+    
+    @IBOutlet weak var SearchBar: UISearchBar!
+    
+    @IBOutlet weak var CalendarButton: UIButton!
+    
+    
+    @IBOutlet weak var AddHomeworkButton: UIButton!
+    
+    
+    @IBOutlet weak var HomeworkLabel: UILabel!
+    
+    
+    @IBOutlet weak var TasksLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
